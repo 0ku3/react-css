af487e2fdf6eb378e752c8d107abe1f217a874c6
0ku3 <103297029+0ku3@users.noreply.github.com>
2022-05-24 17:01:07 +0900

    CSS適用

--- src/components/InlineStyle.jsx
+++ src/components/InlineStyle.jsx
@@ -5,11 +5,19 @@ export const InlineStyle = () => {
     padding: "8px",
     margin: "8px",
     display: "flex",
-    justfyContent: "space-around",
+    justifyContent: "space-around",
     alignItems: "center",
   };
-  const titleStyle = {};
-  const buttonStyle = {};
+  const titleStyle = {
+    margin: 0,
+    color: "#3d84a8",
+  };
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px",
+  };
   return (
     <div style={containerStyle}>
       <p style={titleStyle}>- Inline Style -</p>
